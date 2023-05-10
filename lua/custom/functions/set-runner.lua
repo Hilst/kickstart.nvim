9f7678a6e4348a3fbd640c5aef3b2946987ce5bf
Hilst <hilstfelipe@gmail.com>
2023-05-10 18:59:54 -0300

    auto complete

--- lua/custom/functions/set-runner.lua
+++ lua/custom/functions/set-runner.lua
@@ -1,19 +1,20 @@
 print('parse set-runner')
 
+local runners_table = {
+  gpp = function()
+    require 'custom.functions.runners.gpp'
+  end,
+  golang = function()
+    require 'custom.functions.runners.golang'
+  end,
+  default = function()
+    print('Invalid runner')
+  end
+}
+
 return {
   vim.api.nvim_create_user_command("SetRunner", function(opts)
     local runner = opts.args
-    local runners_table = {
-      gpp = function()
-        require 'custom.functions.runners.gpp'
-      end,
-      golang = function()
-        require 'custom.functions.runners.golang'
-      end,
-      default = function()
-        print('Invalid runner')
-      end
-    }
 
     local runner_cmd = runners_table[runner]
     if runner_cmd then
@@ -21,5 +22,16 @@ return {
     else
       runners_table['default']()
     end
-  end, { nargs = 1 })
+  end, {
+    nargs = 1,
+    complete = function()
+      local keys = {}
+      for key, _ in pairs(runners_table) do
+        if key == 'default' then goto continue end
+        table.insert(keys, key)
+        ::continue::
+      end
+      return keys
+    end
+  })
 }
