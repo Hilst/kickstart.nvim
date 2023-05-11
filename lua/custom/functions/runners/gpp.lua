aa9952f6eba24eef3d120e741bdb16f722b70f35
Hilst <hilstfelipe@gmail.com>
2023-05-11 07:01:53 -0300

    execute gpp

--- lua/custom/functions/runners/gpp.lua
+++ lua/custom/functions/runners/gpp.lua
@@ -1,14 +1,19 @@
-print(
-  [[
+print([[
 Now :Gpp command will run the following:
-  g++ -std=c++17 -I ./*.cpp ./**/*.cpp -o $PWD-exec
-]]
-)
+g++ -std=c++17 -I ./*.cpp ./**/*.cpp -o $PWD/last_dir_name-exec && $PWD/last_dir_name
+]])
 
 return {
   vim.api.nvim_create_user_command("Gpp", function()
     local lib = require 'custom.functions.runners.lib.runners-helper'
-    local cmd = [[g++ -std=c++17 -I ./*.cpp ./**/*.cpp -o $PWD-exec]]
+    local cmd = [[g++ -std=c++17 -I ./*.cpp ./**/*.cpp]]
+
+    local pwd = vim.fn.getcwd()
+    local parts = lib.split_path_string(pwd)
+    local dir = parts.last
+    dir = pwd .. "/" .. string.format("%s_exec", dir)
+    cmd = cmd .. string.format(" -o %s && %s", dir, dir)
+
     local vim_cmd = lib.build_term_split_string(cmd)
     if vim_cmd then
       vim.cmd(vim_cmd)
--- lua/custom/functions/runners/lib/runners-helper.lua
+++ lua/custom/functions/runners/lib/runners-helper.lua
@@ -9,4 +9,29 @@ return {
       return string.format([[ bot vsp | term { %s }]], cmd)
     end
   end,
+  split_path_string = function(path, sep)
+    sep = sep or "/"
+
+    if string.len(path) < 2 then
+      print(string.format([[Invalid path: %s]], path))
+    end
+
+    local r = {}
+    local c = 0
+    local f = ""
+    local l = ""
+    for partition in string.gmatch(path, "([^" .. sep .. "]+)") do
+      if f == "" then f = partition end
+      l = partition
+      table.insert(r, partition)
+      c = c + 1
+    end
+
+    return {
+      parts = r,
+      size = c,
+      first = f,
+      last = l
+    }
+  end
 }
