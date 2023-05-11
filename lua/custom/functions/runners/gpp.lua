d9a8e8fe940840a4228423e9c9d7e5897b823260
Hilst <hilstfelipe@gmail.com>
2023-05-11 08:17:36 -0300

    gpp build folder when running

--- lua/custom/functions/runners/gpp.lua
+++ lua/custom/functions/runners/gpp.lua
@@ -1,17 +1,18 @@
 print([[
 Now :Gpp command will run the following:
-g++ -std=c++17 -I ./*.cpp ./**/*.cpp -o $PWD/last_dir_name-exec && $PWD/last_dir_name
+  $ mkdir -p build
+  $ g++ -std=c++17 -I ./*.cpp ./**/*.cpp -o $PWD/build/last_dir_name-exec && $PWD/build/last_dir_name
 ]])
 
 return {
   vim.api.nvim_create_user_command("Gpp", function()
     local lib = require 'custom.functions.runners.lib.runners-helper'
-    local cmd = [[g++ -std=c++17 -I ./*.cpp ./**/*.cpp]]
+    local cmd = [[mkdir -p build && g++ -std=c++17 -I ./*.cpp ./**/*.cpp]]
 
     local pwd = vim.fn.getcwd()
     local parts = lib.split_path_string(pwd)
     local dir = parts.last
-    dir = pwd .. "/" .. string.format("%s_exec", dir)
+    dir = pwd .. "/build/" .. string.format("%s_exec", dir)
     cmd = cmd .. string.format(" -o %s && %s", dir, dir)
 
     local vim_cmd = lib.build_term_split_string(cmd)
